dadad6ef284ede33c7d7d80e934e98b45bb4f7f8
bluenote <janitachalam@gmail.com>
2023-01-27 15:45:37 -0800

    spec messages

---
+++ src/msg.rs
@@ -0,0 +1,27 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::{Timestamp, Uint128};
+
+#[cw_serde]
+pub struct InstantiateMsg {}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    ExecuteStartAuction {
+        /// The number of participants in the auction.
+        /// Each participant may only bid once.
+        number_of_participants: Uint128,
+        expiration: Timestamp,
+    },
+    /// Allows a participant to bid in the auction.
+    ExecuteBid {
+        /// The participant's bid.
+        bid_amount: Uint128,
+    },
+    /// Allows anyone to end the auction.
+    /// The auction may only be ended once the total number of bids has been received.
+    ExecuteCloseAuction {},
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {}
