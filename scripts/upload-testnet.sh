5a122c81ea0431d53ae7e2295a379474369774f2
bluenote <janitachalam@gmail.com>
2023-03-15 18:16:32 -0700

    add upload script

--- scripts/upload-testnet.sh
+++ scripts/upload-testnet.sh
@@ -1,30 +1,39 @@
-#!/bin/bash
-
 # Set variables
 export CHAIN_ID="uni-6"
 export DENOM="ujunox"
 export BECH32_HRP="juno"
 export CONTRACT_CODE_PATH="artifacts/vcg_auction-aarch64.wasm"
-export CONTRACT_NAME="VCG auction"
+export CONTRACT_NAME="vcg-auction"
 export CONTRACT_INIT_MSG='{}'
-export ADMIN_ADDRESS="juno1873my89qs478e56austefw0ewpp774xmq5m4xv"
+export ADMIN_ADDRESS=""
 export NODE="https://rpc.uni.junonetwork.io:443"
-export KEY_NAME="bluenote"
+export KEY_NAME="<key name>"
 export TXFLAG="--chain-id ${CHAIN_ID} --gas-prices 0.025ujunox --gas auto --gas-adjustment 1.3 --broadcast-mode block"
+export BIDDER_ADDRESS="<bidder address>"
+CONTRACT_ADDRESS=juno1xsv7xkwakhha392s4zq0h7kzdmhqhqd67aknwv3vcy3dhqpw46cq90yvtq
 
 # Store contract code
-# RES=$(junod tx wasm store $CONTRACT_CODE_PATH --from $KEY_NAME $TXFLAG --node $NODE --output json -b block)
+RES=$(junod tx wasm store $CONTRACT_CODE_PATH --from $KEY_NAME $TXFLAG --node $NODE --output json -b block)
 
-# CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[1].value')
-# echo "CODE_ID: " $CODE_ID 
-
-CODE_ID=819
+CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[1].value')
+echo "CODE_ID: " $CODE_ID 
 
 # Instantiate contract
-INIT_MSG='{}'
-junod tx wasm instantiate $CODE_ID "$INIT_MSG" --from $ADMIN_ADDRESS $TXFLAG --node $NODE --label $CONTRACT_NAME
+OUTPUT=$(junod tx wasm instantiate $CODE_ID "$CONTRACT_INIT_MSG" --from $KEY_NAME $TXFLAG --node "https://rpc.uni.junonetwork.io:443" --label $CONTRACT_NAME --admin $ADMIN_ADDRESS)
 
-# # Query contract address
-# CONTRACT_ADDRESS=$(junod query wasm list-contract-by-code $CODE_ID --output json --node $JUNOD_URI | jq -r '.[0] | select(.creator == "'$ADMIN_ADDRESS'") | .address')
+CONTRACT_ADDRESS=$(echo $OUTPUT | jq -r '.logs[0].events[0].attributes[] | select(.key == "_contract_address").value')
+echo "CONTRACT ADDRESS: " $CONTRACT_ADDRESS
 
-# echo "Contract deployed at address: $CONTRACT_ADDRESS"
+# Start auction
+
+junod tx wasm execute $CONTRACT_ADDRESS '{"execute_start_auction": {"name": "test auction", "max_num_participants": "10"}}' --from $KEY_NAME --node "https://rpc.uni.junonetwork.io:443" $TXFLAG
+
+# Bid in auction
+
+junod tx wasm execute $CONTRACT_ADDRESS '{"execute_bid": {"bid_amount": "10"}}' --from $KEY_NAME --node "https://rpc.uni.junonetwork.io:443" $TXFLAG
+
+# Query auction info
+
+junod query wasm contract-state smart $CONTRACT_ADDRESS '{"query_get_bids_for_bidder": {"bidder": "'$BIDDER_ADDRESS'"}}' --node "https://rpc.uni.junonetwork.io:443" 
+
+junod query wasm contract-state smart $CONTRACT_ADDRESS '{"query_get_bids_for_auction": {"auction_id": "1"}}' --node "https://rpc.uni.junonetwork.io:443"
